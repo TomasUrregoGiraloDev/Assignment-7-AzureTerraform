dd98a77d2874220d92eeafc42163ca11bca7b082
Tomas <tomasurregoar2020@gmail.com>
2025-05-16 16:37:07 -0500

    my second commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,30 +1,47 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "d0dc0483-2c3d-4970-afe2-abbf90a1580e"
+}
+
+# Create the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "my-example-rg"
+  location = "westus"
 }
 
-resource "azurerm_app_service_plan" "asp" {
-  name                = var.app_service_plan_name
+# Create the Windows App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "my-example-serviceplan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type             = "Windows"
+  sku_name            = "B1"
 }
 
-resource "azurerm_app_service" "app" {
-  name                = var.app_name
+# Create the Windows Web App
+resource "azurerm_windows_web_app" "webapp" {
+  name                = "my-example-webapp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  service_plan_id     = azurerm_service_plan.appserviceplan.id
+  https_only          = true
 
   site_config {
-    dotnet_framework_version = "v6.0"
-  }
+    minimum_tls_version = "1.2"
+    always_on           = false
 
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+    application_stack {
+      node_version = "~18"  
+    }
   }
+}
+
+# Deploy code from a public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id                  = azurerm_windows_web_app.webapp.id
+  repo_url                = "https://github.com/TomasUrregoGiraloDev/Assignment-7-AzureTerraform"
+  branch                  = "main"
+  use_manual_integration  = true
+  use_mercurial           = false
 }
